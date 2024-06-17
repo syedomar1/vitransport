ee88ae4404f13a6458b0548c0e294cbd6384cddd
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-17 23:35:41 +0530

    Added bg image

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,42 @@
 import "@/styles/globals.css";
+// import Navbar from "../components/Navbar"
+// import Footer from "../components/Footer"
+import Head from "next/head";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import LoadingBar from 'react-top-loading-bar'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [progress, setProgress] = useState(0)
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', ()=>{
+      setProgress(40)
+    })
+    router.events.on('routeChangeComplete', ()=>{
+      setProgress(100)
+    })    
+  }, [router.query])
+  
+  return(
+    <>
+    <LoadingBar
+        color='#e74c3c'
+        progress={progress}
+        waitingTime={400}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      {/* <main className="flex flex-col items-center justify-between">
+        <div className="relative w-full">
+          <div className="absolute -z-10 w-full">
+          <Image src="/vit_bg.png" alt="Background Img" layout="fill" objectFit="cover" />
+          </div>
+        </div>
+      </main> */}
+    <Component {...pageProps} />
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      Hi
+    <div className="relative w-full h-screen overflow-hidden">
+      <div className="absolute inset-0">
+        <Image
+          src="/vit_bg.png"
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          className="blur-sm"
+          quality={100}
+        />
+      </div>
+      <div className="relative z-10 flex flex-col items-center justify-center w-full h-full text-center text-white">
+        <h1 className="text-4xl font-bold">VIT Transport</h1>
+        <p className="mt-4 text-lg">This is a sample text.</p>
+        <p className="mt-4 text-lg">Contact Helpdesk.</p>
+      </div>
     </div>
   );
 }
